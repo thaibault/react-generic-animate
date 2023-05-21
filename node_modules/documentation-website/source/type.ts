2c540c6ef75c5b11ccd38b077e9cfefe1b12a3d5
thaibault <info@torben.website>
2023-05-21 21:01:35 +0200

    Automatic update to new version.

---
+++ node_modules/documentation-website/source/type.ts
@@ -0,0 +1,79 @@
+// -*- coding: utf-8 -*-
+/** @module type */
+'use strict'
+/* !
+    region header
+    [Project page](https://torben.website/website-utilities)
+
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons
+    naming 3.0 unported license.
+    See https://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
+// region imports
+import {ProcedureFunction} from 'clientnode/type'
+import {
+    DomNodes as BaseDomNodes, Options as BaseOptions
+} from 'website-utilities/type'
+
+import Documentation from './index'
+ // endregion
+// region exports
+export type DocumentationFunction =
+    ((..._parameters:Array<unknown>) => unknown) &
+    {class:typeof Documentation}
+declare global {
+    interface JQueryStatic {
+        Documentation:DocumentationFunction
+    }
+}
+
+export type DomNodes<Type = string> =
+    BaseDomNodes<Type> &
+    {
+        aboutThisWebsiteLink:Type
+        aboutThisWebsiteSection:Type
+
+        codeWrapper:Type
+        code:Type
+
+        homeLink:Type
+        mainSection:Type
+
+        headlines:Type
+        tableOfContent:Type
+        tableOfContentLinks:Type
+    }
+
+export interface DefaultOptions {
+    domNodes:DomNodes
+    domNodeSelectorInfix:null|string
+    name:string
+    onExamplesLoaded:ProcedureFunction
+    section:{
+        aboutThisWebsite:{
+            fadeInOptions:JQuery.EffectsOptions<HTMLElement>
+            fadeOutOptions:JQuery.EffectsOptions<HTMLElement>
+        }
+        main:{
+            fadeInOptions:JQuery.EffectsOptions<HTMLElement>
+            fadeOutOptions:JQuery.EffectsOptions<HTMLElement>
+        }
+    }
+    showExample:{
+        domNodeName:string
+        htmlWrapper:string
+        pattern:string
+    }
+}
+export type Options = BaseOptions & DefaultOptions
+// endregion
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
---
+++ playground.tsx
@@ -0,0 +1,46 @@
+// #!/usr/bin/env babel-node
+// -*- coding: utf-8 -*-
+/** @module react-input-material */
+'use strict'
+/* !
+    region header
+    [Project page](https://torben.website/react-material-input)
+
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons
+    naming 3.0 unported license.
+    See https://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
+// region imports
+import Tools from 'clientnode'
+import {useEffect, useState} from 'react'
+import {createRoot} from 'react-dom/client'
+
+import {GenericAnimate} from '.'
+// endregion
+const Application = () => {
+    const [fadeState, setFadeState] = useState<boolean>(false)
+
+    useEffect(():(() => void) => Tools.timeout(
+        ():void => setFadeState((value:boolean):boolean => !value), 2 * 1000
+    ).clear)
+
+    return (<div>
+        <GenericAnimate in={fadeState} timeout={2000}>Fade it!</GenericAnimate>
+
+        <br/>
+
+        <GenericAnimate children="Fade it!" in={!fadeState} timeout={2000}/>
+    </div>)
+}
+window.onload = ():void =>
+    createRoot(document.querySelector('application')!).render(<Application />)
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
