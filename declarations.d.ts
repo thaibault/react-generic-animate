842d7be3ac2253c6c254e9843d7dfc2ccd4fd10b
thaibault <info@torben.website>
2024-07-18 17:10:36 +0200

    Automatic update to new version.

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,6 +1,5 @@
 // -*- coding: utf-8 -*-
 /** @module declarations */
-/// <reference path="./node_modules/weboptimizer/declarations" />
 /*
  NOTE: Already defined in weboptimizer's generic declaration. but currently
  only needed for intellij only.
